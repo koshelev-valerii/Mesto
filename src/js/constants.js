5d7fb4f6a5cf6b267bb242ece525dbc53d1197bd
Valerii Koshelev <58371774+koshelev-valerii@users.noreply.github.com>
2019-12-13 17:14:34 +0100

    Set up the site

---
+++ src/js/constants.js
@@ -0,0 +1,83 @@
+export const profile = document.querySelector('.profile');
+
+export const placesList = document.querySelector('.places-list');
+
+export const buttonClose = document.querySelector('.popup__close');
+
+export const buttonCloseNew = document.querySelector('.popup__close-new');  
+
+export const placesCard = document.querySelector('.places-card');
+
+export const popupNew = document.querySelector('.popup__new'); 
+
+export const buttonAddNew = document.querySelector('.button-new'); 
+
+export const popupButton = document.querySelector('.popup__button');
+
+export const popupEdit = document.querySelector('.popup__edit'); 
+
+export const popupAvatar = document.querySelector('.popup__avatar'); 
+
+export const editAvatar = document.querySelector('.button-avatar');
+
+export const editProfile = document.querySelector('.button-edit');
+
+export const buttonCloseEdit = document.querySelector('.popup__close-edit');
+
+export const img = document.createElement('img');
+
+export const popupImg = document.querySelector('.popup__img'); 
+
+export const popupArea = document.querySelector('.popup__area');
+
+export const buttonCloseImg = document.querySelector('.popup__close-img');  
+
+export const userName = document.querySelector('.user-info__name'); 
+
+export const userJob = document.querySelector('.user-info__job'); 
+
+export const userId = document.querySelector('.user-info__id'); 
+
+export const userAvatar = document.querySelector('.user-info__photo'); 
+
+export const editName = document.querySelector('.edit__name'); 
+
+export const editJob = document.querySelector('.edit__job');
+
+export const buttonNew = document.querySelector('.popup__button-new'); 
+
+export const buttonEdit = document.querySelector('.popup__button-edit');
+
+export const buttonAvatar = document.querySelector('.popup__button-avatar'); 
+
+export const form = document.forms.new;
+
+export const formEdit = document.forms.edit;
+
+export const formAvatar = document.forms.avatar;
+
+export const input = document.querySelector('.popup__input'); 
+
+export const inputNew1 = document.querySelector('.popup__input-new1');
+
+export const inputNew2 = document.querySelector('.popup__input-new2');
+
+export const inputEdit1 = document.querySelector('.popup__input-edit1');
+
+export const inputEdit2 = document.querySelector('.popup__input-edit2');
+
+export const inputAvatar = document.querySelector('.popup__input-avatar');
+
+export const inputNameImg = form.elements.nameImg;
+
+export const inputLink = form.elements.link;
+
+export const inputName = formEdit.elements.name;
+
+export const inputJob = formEdit.elements.job;
+
+export const inputLinkAvatar = formAvatar.elements.link;
+
+export const errorMessage = document.querySelector('.error-message');
+
+export const partOfLink = /(^https?:\/\/)?[a-z0-9~_\-\.]+\.[a-z]{2,9}(\/|:|\?[!-~]*)?/i;
